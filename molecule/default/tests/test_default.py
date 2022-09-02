43a62196c9bdb569f8c2f312cebdfcdb2e8dab7f
Bo.Sch <bodo@boone-schulz.de>
2022-09-02 09:11:53 +0200

    support and tests for artixlinux (#3)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,37 +15,50 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def base_directory():
     cwd = os.getcwd()
 
-    if('group_vars' in os.listdir(cwd)):
+    if 'group_vars' in os.listdir(cwd):
         directory = "../.."
         molecule_directory = "."
     else:
         directory = "."
-        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+        molecule_directory = f"molecule/{os.environ.get('MOLECULE_SCENARIO_NAME')}"
 
     return directory, molecule_directory
+
+
+def read_ansible_yaml(file_name, role_name):
+    """
+    """
+    read_file = None
+
+    for e in ["yml", "yaml"]:
+        test_file = f"{file_name}.{e}"
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return f"file={read_file} name={role_name}"
 
 
 @pytest.fixture()
 def get_vars(host):
     """
-
     """
     base_dir, molecule_dir = base_directory()
     distribution = host.system_info.distribution
+    operation_system = None
 
     if distribution in ['debian', 'ubuntu']:
-        os = "debian"
+        operation_system = "debian"
     elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
-        os = "redhat"
-    elif distribution in ['arch']:
-        os = "archlinux"
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
 
-    print(" -> {} / {}".format(distribution, os))
-
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
-    file_distibution = "file={}/vars/{}.yaml name=role_distibution".format(base_dir, os)
+    file_defaults = read_ansible_yaml(f"{base_dir}/defaults/main", "role_defaults")
+    file_vars = read_ansible_yaml(f"{base_dir}/vars/main", "role_vars")
+    file_distibution = read_ansible_yaml(f"{base_dir}/vars/{operation_system}", "role_distibution")
+    file_molecule = read_ansible_yaml(f"{molecule_dir}/group_vars/all/vars", "test_vars")
+    # file_host_molecule = read_ansible_yaml(f"{base_dir}/host_vars/{HOST}/vars", "host_vars")
 
     defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
     vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
@@ -75,23 +88,25 @@ def test_installed_packages(host):
 
     if distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
         package = "nagios-plugins"
-    if distribution == 'arch':
+    if distribution in ['arch', 'artix']:
         package = "monitoring-plugins"
 
-    p = host.package(package)
-    assert p.is_installed
-    assert p.version.startswith("2")
+    if not distribution == "artix":
+        p = host.package(package)
+        assert p.is_installed
+        assert p.version.startswith("2")
 
 
 def test_blacklisted_packages(host, get_vars):
     """
-
     """
+    distribution = host.system_info.distribution
     blacklist = get_vars.get('monitoring_plugins_blacklist')
 
-    if(blacklist and len(blacklist) != 0):
-        p = host.package(blacklist[0])
-        assert not p.is_installed
+    if not distribution == "artix":
+        if blacklist and len(blacklist) != 0:
+            p = host.package(blacklist[0])
+            assert not p.is_installed
 
 
 def test_generic_directory(host):
@@ -101,7 +116,7 @@ def test_generic_directory(host):
 
     assert d.exists
 
-    if(distribution == 'arch'):
+    if distribution in ['arch', 'artix']:
         assert d.is_directory
     else:
         assert d.is_symlink
